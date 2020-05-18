4495aad08e3d3113d65e110c5e779e687a900615
NamNH <>
2020-05-18 08:53:55 +0700

    update key

--- SeChi/SeChi/Constants/HMConstants.swift
+++ SeChi/SeChi/Constants/HMConstants.swift
@@ -11,6 +11,6 @@ import UIKit
 class HMConstants {
     static let chatBotToken = "a3651a25d3ed1d9e2f7603b765ceff6e"
     static let speechToTextAPIKey = "31zMkJ2X6jnX00bpvBSVqM7z10NvP1Ty"
-    static let viettelSpeechToTextAPIKey = "q6AoI7lnPyGRGFTW0J3YHKOoW5M-KhAnzepfSBMD-TS5d9SqofGJxs9Aj20aFTkd"
+    static let viettelSpeechToTextAPIKey = "0Hfm-YHBXHJYvAE0utNxNupEnZDFQPELFQEN3Bo7gXMewxbzwXEU8a14g7nuUKPf"
     static let oneSignalAPIKey = "3923864c-d8ad-4fcd-9fb1-941f45923898"
 }
